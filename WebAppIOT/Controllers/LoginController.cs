c2815f8f52d324bbdb0db7207e1e60354f827ca0
TongAntonio <adum_tong.net@hotmail.com>
2022-05-19 05:21:58 +0700

    test
    test

---
+++ WebAppIOT/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppIOT.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAppIOT.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CheckLogin(Login login)
+        {
+            if (login.Username == "admin" && login.Password == "1234")
+            {
+                return RedirectToAction("Index", "Menu");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebAppIOT/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WebAppIOT.Models
+{
+    public class Login
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string grant_type { get; set; }
+    }
+}
